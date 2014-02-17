5f898174c3ad1aea84af395690b951833e173d34
rbsexton <robertbsexton@gmail.com>
2014-02-17 08:56:25 -0800

    Notes.

--- sapi/userapp.c
+++ sapi/userapp.c
@@ -25,18 +25,52 @@ int user_app_present(unsigned long addr) {
 
 ///
 /// @brief Assembly Wrapper that loads up SP and PC and jumps to the app.
-///
+/// Callable from thread mode.  Restarting the app from exception mode requires the 
+/// next function
 void user_app_launch(unsigned long addr) {
 
      // Allow the client to own the whole thing.
      __asm(    "@ Base Address of next Image is in r0\n"
 	     "ldr r1, [r0]   @ This should be the stack pointer\n"
-	     "mov sp, r1    @ Load it into the process sp\n"
+	     "msr psp, r1    @ Load it into the process sp\n"
 	     "ldr r1,[r0, #4] @ Load up the program counter\n"
 	     "mov pc,r1 @ branch to official code\n"
 	     );
 
 } 
+
+///
+/// @brief Assembly wrapper to start the app by exiting from exception mode.
+/// The basic trick here is to create a custom stack frame and cleanup the master stack.
+///
+/// This doesn't actually work as a function, but if you use this code in a bare 
+/// PendSV handler, after de-pending the PendSV, it will.
+/// The crucial part is falling off the end with a bx lr
+/// The stack type check is so it'll work when called repeatedly.
+///
+/// Ideally, this would first call some high-level code that would do it's thing
+/// and leave the stack in a clean state.
+
+void user_app_exception_launch(unsigned long addr) {
+    __asm(
+	  "TST 	LR,#0x04        @ Check to see which stack\n"
+	  "it     eq             \n"
+	  "addeq   sp, #32       @ Reset the supervisor stack frame\n"
+
+          "ORR   lr, lr, #4    @ Return into thread mode from PSP\n"
+	        	
+	  "ldr   r0, =0x08008000        \n"
+          "ldr   r1, [r0]      @ New SP\n"
+          "sub   r1, #32       @ Point it at the bottom of a fake stack frame\n"
+          "msr   psp, r1       @ Load it into the process sp\n"
+
+          "ldr   r3, [r0, #4]  @ New PC\n"
+          "str   r3, [r1, #24] @ Into the new stack frame\n"
+	  
+	  "ldr   r2, = 0x01000000 @ XPSR Thumb mode\n"
+          "str   r2, [r1, #28] @ Into the stack\n"
+	  );
+}
 
 // Close the Doxygen group
 //!
--- sapi/userapp.h
+++ sapi/userapp.h
@@ -1,3 +1,4 @@
 int user_app_present(unsigned long);
 void user_app_launch(unsigned long);
+void user_app_exception_launch(unsigned long);
 
