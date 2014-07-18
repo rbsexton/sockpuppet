65a6604f8979d13d3fdc74eeecf03d39c6063d96
rbsexton <robertbsexton@gmail.com>
2014-07-18 08:45:58 -0700

    Define the forth name as a fixed-length array. Fix a compiler warning.

--- sapi/sapi-dylink.h
+++ sapi/sapi-dylink.h
@@ -13,7 +13,7 @@
 /// 
 
 // This could be packed more tightly, but its now exactly
-// 16 bytes per record, which is nice.  Make sure that doesn't
+// 16 bytes per record, which is nice.  Make sure that doesnt
 // change.
 
 // Conventions: 
@@ -21,6 +21,10 @@
 // and count is the number of them.
 // char array[64] = 1,64
 // struct { a, b } s; sizeof(s),1
+
+// Adjust the size of the string so you get a 32-byte record.
+
+#define DYNLINKNAMEMLEN 22
 
 typedef struct {
 	// This union is a bit crazy, but its the simplest way of 
@@ -31,17 +35,17 @@ typedef struct {
 		unsigned int    ui;
 		unsigned int*  uip;
 		unsigned long* ulp;
-		} p;   ///< Pointer to the object of interest
-	int16_t size;  ///< Size in bytes
-	int16_t count; ///< How many
-	int8_t kind;   ///< Is this a variable or a constant?
-	uint8_t strlen;   ///< Length of the string
-	char  name[]; ///< Null-Terminated C string.
+		} p;   ///< Pointer to the object of interest (4)
+	int16_t size;  ///< Size in bytes (6)
+	int16_t count; ///< How many (8)
+	int8_t kind;   ///< Is this a variable or a constant? (9)
+	uint8_t strlen;   ///< Length of the string (10)
+	const char name[DYNLINKNAMEMLEN]; ///< Null-Terminated C string.
 	} runtimelink_t;
 
 
 /// Helper Macro.
-#define FORTHNAME(x) ( (int8_t) sizeof(x)-1), (x)
+#define FORTHNAME(x) ( (int8_t) sizeof(x)-1), x
 
 extern const runtimelink_t dynamiclinks[];
 
