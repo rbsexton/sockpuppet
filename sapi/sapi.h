22f305216efce3d2275f0416927124f0bda6aa06
rbsexton <robertbsexton@gmail.com>
2014-07-18 08:48:47 -0700

    Define the millisecond counter.

--- sapi/sapi.h
+++ sapi/sapi.h
@@ -1,13 +1,16 @@
-// sapi.h
-// Required headers for compilation.
-// 
+/// \file sapi.h
+/// \brief Required headers for compilation.
+
+#ifndef __STDINT_H__
+#include <stdint.h>
+#endif
+
+#include "sapiopts.h"  // The official place for local customization.
 
 // Needed for user app launch.
 extern uint32_t app_restart_requested;
 extern void launchuserapp(uint32_t *);
 
-// Forth does not actually require a MS counter in the basic config, 
-// but I consider it to be a requirement.
-// Networking stacks will be doing this anyway.
-
-extern volatile MSCOUNTER
+// MPE Forth does not actually require a MS counter in the basic config, 
+// but I consider it to be a requirement.  Networking stacks will be doing this anyway.
+extern volatile uint32_t MSCOUNTER;
