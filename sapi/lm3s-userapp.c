3ea3a6ce13c51a58c3796022d530f2b90c44e14b
rbsexton <robertbsexton@gmail.com>
2014-09-13 23:40:38 -0700

    Add reset via NVIC.

--- sapi/lm3s-userapp.c
+++ sapi/lm3s-userapp.c
@@ -1,6 +1,6 @@
 ///
 /// @file lm3s-userapp.c
-/// @brief Launch Code and PendSV handler for starting up forth.
+/// @brief Launch Code and PendSV handler for starting up forth, plus system reset.
 /// Code to setup PendSV varies by platform/library.
 
 #include <stdint.h>
@@ -17,5 +17,14 @@ void launchuserapp(uint32_t *frame) {
 	HWREG(NVIC_INT_CTRL) = NVIC_INT_CTRL_PEND_SV;
 	app_restart_requested = frame[0];
 	}
+
+// This is a NVIC operation, but it's described 
+// differently in different compilation environments
+void __SAPI_08_NVICReset(uint32_t *frame) {
+	// HWREG(0xe000ed0c) = 0x05fa0004;
+	HWREG(NVIC_APINT) = 0x05fa0004;
+	return;
+	}
+
 
 
