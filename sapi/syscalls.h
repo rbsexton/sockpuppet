bcc8c3eaa95f9be17e3ff3dbf9d7dba5c8c6df6e
rbsexton <robertbsexton@gmail.com>
2016-05-02 09:10:32 -0700

    Create an include file to help enforce the arguments to the bare sys calls.

---
+++ sapi/syscalls.h
@@ -0,0 +1,17 @@
+// Function Prototypes for SAPI Calls.
+// This serves as a basic check for supplied supervisor functions.
+
+// Applies to SAPI ABI 2.1
+
+void SVC_Handler();
+
+uint32_t __SAPI_00_ABIVersion();
+long* __SAPI_01_GetRuntimeData(int which);
+int __SAPI_02_PutChar(int stream, uint8_t c, unsigned long *tcb);
+int __SAPI_03_GetChar(int stream,unsigned long *tcb);
+int __SAPI_04_GetCharAvail(int stream);
+int __SAPI_05_PutString(int stream, uint8_t *p, int count, unsigned long *tcb);
+int __SAPI_06_EOL(int stream);
+unsigned __SAPI_13_CPUUsage();
+void __SAPI_14_PetWatchdog(unsigned howmuch);
+unsigned __SAPI_15_GetTimeMS(int whatform);
