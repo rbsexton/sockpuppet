c9f6a9b39856b3bcd7b745c1385ae8d78d293d05
rbsexton <robertbsexton@gmail.com>
2020-03-03 18:25:54 -0800

    Re-organize the level 2 driver so that we don’t bypass the ring buffer.

--- target/stm32_usart_level2.c
+++ target/stm32_usart_level2.c
@@ -69,7 +69,7 @@ static const unsigned int  uarts[] = {
 #endif 
 
 #ifndef USART_OUTPUT_RBSIZE
-#define USART_OUTPUT_RBSIZE 128 
+#define USART_OUTPUT_RBSIZE 256 
 #endif 
 
 
@@ -162,30 +162,21 @@ static int __SAPI_PutCharGeneral_USART(
       return(-1);
       }
     }
-    
+      
   // The data will go in the ringbuffer, or the FIFO
-  // At this point we know that there is room for it. 
-  // Make a best effort to add data to the hardware fifo, then 
-  // decide what to do.    We're usually writing faster than 
-  // the USART can go, so the common case is ringbuffer add. 
+  // At this point we know that there is room for it.
+  // Always put the data into the ringbuffer.  That 
+  // also helps with debug.  
   
-  // If the hardware FIFO is full it goes into the ring buffer. 
-  if ( ! LL_USART_IsActiveFlag_TXE((USART_TypeDef *) base) ) {
-    ringbuffer_addchar(ring, c);      
-    return(ringbuffer_free(ring));      
-    }
-  
-  // Otherwise we pull from the ringbuffer and send that, 
-  // or we send the data direct to the hardware.  
-  if ( ringbuffer_used(ring) ) { // Send the stuff from the ring.
-    LL_USART_TransmitData8( (USART_TypeDef *) base,ringbuffer_getchar(ring));
-    ringbuffer_addchar(ring, c);      
-    } 
-  else { // Empty Ringbuffer.   Straight to hardware. 
-      LL_USART_TransmitData8( (USART_TypeDef *) base,c);
+  // Put the data into the ringbuffer. 
+  ringbuffer_addchar(ring, c);      
+    
+  // If there is room in the FIFO, transmit  
+  if ( LL_USART_IsActiveFlag_TXE((USART_TypeDef *) base) ) {
+    LL_USART_TransmitData8( (USART_TypeDef *) base,ringbuffer_getchar(ring));  
     }
 
-  return(ringbuffer_free(ring));        
+  return(ringbuffer_free(ring));  
   }
 
 int __SAPI_02_PutChar_USART(int c, int stream) {
